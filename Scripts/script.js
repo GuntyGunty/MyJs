70f2110ed099edbe440bb78f456f17939c1e8f25
a.beliaeva <a.beliaeva@WSCR-59>
2017-10-25 18:06:49 +0300

    Новая структура листа

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,150 +1,152 @@
-﻿//import addDefaultListener from './blocks/add-default-listener.js';
-//import noJs from './blocks/remove-no-js.js';
-
-////addDefaultListener();
-//noJs();
-
-//const block = document.querySelector(`.block`).innerHTML;
-
-//console.log(block);
-const selects = document.querySelectorAll(".drop-list__list");
-const options = document.querySelectorAll(".drop-list__item");
-const itemObject = {};
+﻿const selects = document.querySelectorAll(".drop-list__list");
+const lists = document.querySelectorAll(".drop-list__list-second");
+const inputs = document.querySelectorAll(".drop-list__input");
+const radioBlocks = document.querySelectorAll(".radio__block");
 const myObject = {};
 myObject.selects = [];
+myObject.inputs = [];
+myObject.radioBlocks = [];
+myObject.lists = [];
 
 Array.from(selects, (select, index) => {
-	myObject.selects.push({});
+
+	myObject.selects.push({})
 	myObject.selects[`${index}`].id = select.id;
-	myObject.selects[`${index}`].title = select.previousElementSibling.textContent || select.previousElementSibling.innerText;
-	myObject.selects[`${index}`].selectType = select.selectType;
+	myObject.selects[`${index}`].selectId = select.selectId;
+	myObject.selects[`${index}`].selectType = false;
+	myObject.selects[`${index}`].selectName = select.selectName;
 	myObject.selects[`${index}`].formID = select.form.id;
 	myObject.selects[`${index}`].name = select.name;
 	myObject.selects[`${index}`].options = [];
 	Array.from(select.options, (option, i) => {
 		myObject.selects[`${index}`].options.push({});
 		myObject.selects[`${index}`].options[`${i}`].value = option.value;
-		myObject.selects[`${index}`].options[`${i}`].text = option.textContent || option.innerText;
+		myObject.selects[`${index}`].options[`${i}`].text = option.text;
 		myObject.selects[`${index}`].options[`${i}`].selected = option.selected;
-	});
-
+	})
 });
-setTimeout(() => {
-	console.log(myObject);
-}, 500);
+
+Array.from(inputs, (input, ind) => {
+
+	myObject.inputs.push({})
+	myObject.inputs[`${ind}`].id = input.id;
+	myObject.inputs[`${ind}`].name = input.name;
+	myObject.inputs[`${ind}`].placeholder = input.placeholder;
+	myObject.inputs[`${ind}`].type = input.type;
+});
 
 
-const renderListItems = () => {
+Array.from(lists, (list, elem) => {
+	myObject.lists.push({})
+	myObject.lists[`${elem}`].id = list.id;
+	myObject.lists[`${elem}`].name = list.name;
+	myObject.lists[`${elem}`].selectType = true;
+	myObject.lists[`${elem}`].options = [];
+	Array.from(list.options, (option, el) => {
+		myObject.lists[`${elem}`].options.push({});
+		myObject.lists[`${elem}`].options[`${el}`].value = option.value;
+		myObject.lists[`${elem}`].options[`${el}`].text = option.text;
+	})
+});
 
-	//const selectOptions = myObject.selects[0];
-	//console.log(selectOptions);
-	//	selectOptions.map((option) => {
-	//		console.log(option);
-			return (
-				`<ul class="drop-list__list no-js" id="list__id__${myObject.selects.id}" >
-				<li class="drop-list__item">
-					<input class="drop-list__radio" id="radio_id_${myObject.selects.id}_1" type="${myObject.selects.selectType}" name="drop_list_${myObject.selects.id}" />
-					<label class="drop-list__label drop-list__label--list" for="radio_id__1">${myObject.selects.options.text}</label>
-				</li>
-			</ul>`
-			);
-	/*	})*/
+Array.from(radioBlocks, (block, item) => {
+	myObject.radioBlocks.push({})
+	myObject.radioBlocks[`${item}`].id = block.id;
+	myObject.radioBlocks[`${item}`].buttons = [];
+	const blockButtons = block.querySelectorAll(`.drop-list__radio`);
+	Array.from(blockButtons, (button, it) => {
+		console.log(button);
+		myObject.radioBlocks[`${item}`].buttons.push({});
+		myObject.radioBlocks[`${item}`].buttons[`${it}`].id = button.id;
+		myObject.radioBlocks[`${item}`].buttons[`${it}`].type = button.type;
+	});
+});
+
+console.log(myObject);
+
+const typeitemsFunc = (selectType) => {
+	if (selectType === true) {
+			return `checkbox`;
+		} else {
+			return `radio`;
+		}
+};
+
+console.log(typeitemsFunc());
+
+const renderAll = () => {
+	return Array.from(myObject.selects).map((select, index) => {
+		return renderDiv(index);
+	});
 }
 
+const renderDiv = (index) => {
+	return (
+		`<div class="block">
+			${renderInputs(index)}
+			${renderList(index)}
+			${renderRadio(index)}
+		</div>`
+	);
+};
 
-document.querySelector(`.block`).innerHTML = renderListItems();
 
-//block = renderListItems();
+const renderInputs = (index) => {
+	const input = myObject.inputs[index];
 
-//${myObject.selects.options.value }
-//${myObject.selects.options.text }
+	return (
+		`<input class="drop-list__input" id="${input.id}" type="${input.type}" name="${input.name}" placeholder="${input.placeholder}">`
+	);
+
+};
+
+const renderRadioBlock = () => {
+	return (
+		`<div class="drop-list__list no-js">
+			${renderRadio(myObject.radioBlocks[index].buttons).join(``)}
+		</div>`
+		);
+}
+
+const renderRadio = (index) => {
+	const radio = myObject.radioBlocks[`${index}`].buttons[`${index}`];
+	return (
+		`<input class="radio__input" id="${radio.id}" type="${radio.type}">
+		<input class="radio__input" id="${radio.id}" type="${radio.type}">
+		<input class="radio__input" id="${radio.id}" type="${radio.type}">`
+		)
+}
+
+const renderList = (index) => {
+	return (
+		`<ul class="drop-list__list no-js">
+			${renderListItems(myObject.selects[index].options).join(``)}
+		</ul>`
+	);
+};
 
 
-//const myObject = {
-//	selects: [
-//		{
-//			title: `Простой список`,
-//			selectId: `selectID`,
-//			selectType: `selectType`,
-//			selectName: `selectName`,
-//			placeHolder: `Пожалуйста, введите текст`,
-//			formID: `formID`,
-//			type: `text`,
-//			name: `name`,
-//			options: [
-//				{
-//					value: `value`,
-//					text: `text`,
-//					tabindex: `0`,
-//					selected: false
-//				}
-//			]
-//		}
-//	]
-//};
+console.log(typeitemsFunc());
 
-//const typeitemsFunc = () => {
-//	if (myObject.selects[0].type === `multiple`) {
-//		return `checkbox`;
-//	} else {
-//		return `radio`;
-//	}
-//};
+const renderListItems = (options) => {
+	return options.map((option) => {
+		return (
+			`<li class="drop-list__item">
+				<input class="drop-list__radio" id="radio_id_${option.value}_1" type="${typeitemsFunc()}" name="drop_list_${option.value}" />
+				<label class="drop-list__label drop-list__label--list" for="radio_id__1">${option.text}</label>
+			</li>`
+		);
+	});
+};
 
-//const selectFunc = () => {
-//	if (myObject.selects[0].options.checkes === true) {
-//		return `false`;
-//	} else {
-//		return `true`;
-//	}
-//};
 
-//const selectOptions = myObject.selects[0].options;
+console.log(renderAll());
 
-//const renderListItems = (selectOptions) => {
-//	return selectOptions.map((option) => {
-//		return (
-//			`<li class="drop-list__item">
-//				<input class="drop-list__radio" id="radio_id_${myObject.selects[0].options[0].value}_1" type="${typeitemsFunc()}" name="drop_list_${myObject.selects[0].options[0].value}" selected="${selectFunc()}" />
-//				<label class="drop-list__label drop-list__label--list" tabindex="${myObject.selects[0].options[0].tabindex} " for="radio_id_${myObject.selects[0].options[0].value}_1">${myObject.selects[0].options[0].text}</label>
-//			</li>`
-//		);
-//	});
-//}
+document.querySelector(`.block`).innerHTML = renderAll().join(``);
 
-//const renderHTML = () => {
-//	return (
-//		`<section class="block block--1">
-//		<div class="block__toggle">
-//			<button class="block__button">
-//				<svg class="block__svg" width="15" height="15">
-//				<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-open"></use>
-//				</svg >
-//				Раскрыть блок
-//			</button >
-//			<h2 class="block__header">${myObject.selects[0].title}</h2>
-//		</div >
-//	<div class="block__body">
-//		<div class="drop-list">
-//			<div class="drop-list__input-block">
-//			<label class="drop-list__label-title drop-list__label--input" for="${myObject.selects[0].selectId}">Введите данные</label>
-//			<div class="drop-list__inner">
-//				<input class="drop-list__input" id="${myObject.selects[0].selectId}" type="${myObject.selects[0].type}" name="${myObject.selects[0].name}" placeholder="${myObject.selects[0].placeHolder}">
-//				<button class="drop-list__button">
-//					<svg class="drop-list__svg" width="25" height="13">
-//						<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-open-list"></use>
-//					</svg>
-//					Открыть список
-//				</button>
-//			</div>
-//		</div>
-//		<ul class="drop-list__list no-js">
-//			${renderListItems().join(`,`)}
-//		</ul>
-//		</div>
-//	</div>
-//</section>`
-//	);
-//};
-//console.log()
-//document.getElementsByTagName(`body`)[0].innerHTML = renderHTML();
+
+
+
+
+
+
